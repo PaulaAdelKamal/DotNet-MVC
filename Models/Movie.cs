afcf42f331a80a36c14c6049c74eb57e145f3ab6
paula adel <31236845+PaulaAdelKamal@users.noreply.github.com>
2022-11-17 13:50:12 +0200

    Add files via upload

---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site101.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(250)]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public double Rate { get; set; }
+
+        [Required, MaxLength(2500)]
+        public string Storyline { get; set; }
+
+        [Required]
+        public byte[] poster { get; set; }
+
+        public byte GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
---
+++ ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using site101.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site101.ViewModels
+{
+    public class MovieFormViewModel
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="hmdaaa says: This is a required field"), StringLength(250)]
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        [Range(1,10)]
+        public double Rate { get; set; }
+
+        [Required, StringLength(2500)]
+        public string Storyline { get; set; }
+        
+        [Display(Name = "Choose poster")]
+        public byte[] poster { get; set; }
+
+        [Display (Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
